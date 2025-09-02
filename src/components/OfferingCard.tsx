8531aa3c635f3d063fb260aef6ae72f5dd5d012c
ksweat48 <ksweat48@gmail.com>
2025-09-02 09:23:28 -0400

    Updated OfferingCard.tsx

--- src/components/OfferingCard.tsx
+++ src/components/OfferingCard.tsx
@@ -288,7 +288,10 @@ const OfferingCard: React.FC<{
           {/* Main Text: Offering name with price inline */}
           <div className="flex items-baseline justify-between">
             <h6 className="font-poppins font-bold text-black text-sm line-clamp-1 flex-1 mr-2">
-              {business.offeringTitle || business.title || 'Untitled Offering'}
+             {business.isAIGenerated && searchQueryText 
+               ? `Serves ${searchQueryText}` 
+               : (business.offeringTitle || business.title || 'Untitled Offering')
+             }
             </h6>
           </div>
           
